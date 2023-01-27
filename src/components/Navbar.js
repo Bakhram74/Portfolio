54ba5cdfe2b9ab00ab89c38bf8846df42ee70e05
Bakhram <bakhram7493@gmail.com>
2023-01-27 08:36:58 +0300

    added CV & refactoring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
+
 import {
   AiFillStar,
   AiOutlineHome,
@@ -38,19 +37,20 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
+        {/*<Navbar.Brand href="/" className="d-flex">*/}
+        {/*  /!*<img src={logo} className="img-fluid logo" alt="brand" />*!/*/}
+        {/*  <span style={{}}>BA</span>*/}
+        {/*</Navbar.Brand>*/}
+        {/*<Navbar.Toggle*/}
+        {/*  aria-controls="responsive-navbar-nav"*/}
+        {/*  onClick={() => {*/}
+        {/*    updateExpanded(expand ? false : "expanded");*/}
+        {/*  }}*/}
+        {/*>*/}
+        {/*  <span></span>*/}
+        {/*  <span></span>*/}
+        {/*  <span></span>*/}
+        {/*</Navbar.Toggle>*/}
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
@@ -91,20 +91,9 @@ function NavBar() {
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Bakhram74"
                 target="_blank"
                 className="fork-btn-inner"
               >
